31a25588d3d57f964319a30ee3499e798a80e293
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2019-11-21 14:25:41 +0530

    update menu items to navigate withRouter

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import "./homepage.styles.scss";
-import Directory from './../../components/directory/directory.component';
-const HomePage = () => (
+import Directory from "./../../components/directory/directory.component";
+const HomePage = ({ history }) => (
   <div className="homepage">
-    
-      <Directory/>
+    <Directory />
   </div>
 );
 
