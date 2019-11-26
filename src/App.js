e22cbe5b0539857b97326fc57421adf66d0afb1c
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2019-11-26 17:31:27 +0530

    added checkout item component, checkout page, reselect for places

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from './pages/checkout/checkout.component';
 import { setCurrentUser } from "./redux/user/user.actions";
 import "./App.css";
-
+import {selectCurrentUser} from './redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 class App extends Component {
   unsubscribeFromAuth = null;
   componentDidMount() {
@@ -37,6 +39,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -54,8 +57,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
