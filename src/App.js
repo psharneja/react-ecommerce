d777baa61733d4b8e64e4049f5fdefa39e259465
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2019-12-30 15:41:33 +0530

    root shop user cart sagas

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import CheckoutPage from './pages/checkout/checkout.component';
-import { setCurrentUser } from "./redux/user/user.actions";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import "./App.css";
-import {selectCurrentUser} from './redux/user/user.selectors';
-import { createStructuredSelector } from 'reselect';
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
+import { checkUserSession } from "./redux/user/user.actions";
+
 // import { selectCollectionsforPreview } from "./redux/shop/shop.selectors";
 
 class App extends Component {
   unsubscribeFromAuth = null;
   componentDidMount() {
-    const { setCurrentUser } = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot(snapShot => {
-          this.props.setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
-          });
-        });
-      }
-      setCurrentUser(userAuth); 
-      // console.log(collectionsArray.map(({title, items})=> ({title, items})))
-      // addCollectionAndDocuments('collections', collectionsArray.map(({title, items})=> ({title, items})));
-    });
+    const {checkUserSession} = this.props;
+    checkUserSession();
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    //   if (userAuth) {
+    //     const userRef = await createUserProfileDocument(userAuth);
+    //     userRef.onSnapshot(snapShot => {
+    //       this.props.setCurrentUser({
+    //         id: snapShot.id,
+    //         ...snapShot.data()
+    //       });
+    //     });
+    //   }
+    //   setCurrentUser(userAuth);
+    //   // console.log(collectionsArray.map(({title, items})=> ({title, items})))
+    //   // addCollectionAndDocuments('collections', collectionsArray.map(({title, items})=> ({title, items})));
+    // });
   }
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -62,12 +63,11 @@ class App extends Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
+  currentUser: selectCurrentUser
   // collectionsArray: selectCollectionsforPreview
 });
-
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  checkUserSession: () => dispatch(checkUserSession())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { auth } from "./../../firebase/firebase.utils";
+// import { auth } from "./../../firebase/firebase.utils";
 import CartIcon from "./../cart-icon/cart-icon.component";
 import CartDropDown from "./../cart-dropdown/cart-dropdown.component";
 import { ReactComponent as Logo } from "./../../assets/crown.svg";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
+import { signOutStart } from "../../redux/user/user.actions";
 import {
   HeaderContainer,
   LogoContainer,
@@ -16,26 +17,18 @@ import {
 } from "./header.styles";
 import "./header.styles.scss";
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
   <HeaderContainer>
     <LogoContainer to="/">
       <Logo className="logo" />
     </LogoContainer>
     <OptionsContainer>
-      <OptionLink  to="/shop">
-        SHOP
-      </OptionLink>
-      <OptionLink  to="/contact">
-        CONTACT
-      </OptionLink>
+      <OptionLink to="/shop">SHOP</OptionLink>
+      <OptionLink to="/contact">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>
-          SIGN OUT
-        </OptionDiv>
+        <OptionDiv onClick={signOutStart}>SIGN OUT</OptionDiv>
       ) : (
-        <OptionLink  to="/signin">
-          SIGN IN
-        </OptionLink>
+        <OptionLink to="/signin">SIGN IN</OptionLink>
       )}
       <CartIcon />
     </OptionsContainer>
@@ -48,4 +41,8 @@ const mapStateToProps = createStructuredSelector({
   hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+  signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
