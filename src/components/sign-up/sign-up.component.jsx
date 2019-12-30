390a93db0555ba1ebb2e5ba9c87e490bc0dc2f45
Prabhsimran Singh (Simar Harneja) <psharneja@gmail.com>
2019-12-30 16:06:23 +0530

    signup sagas

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,10 +1,8 @@
 import React from "react";
+import {connect} from 'react-redux';
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import {
-  auth,
-  createUserProfileDocument
-} from "./../../firebase/firebase.utils";
+import {signUpStart} from '../../redux/user/user.actions';
 import './sign-up.styles.scss';
 
 
@@ -21,26 +19,28 @@ class SignUp extends React.Component {
 
     handleSubmit = async event => {
         event.preventDefault();
+        const {signUpStart} = this.props;
         const {displayName, email, password, confirmPassword} =this.state;
 
         if(password !== confirmPassword) {
             alert(`Passwords don't match`);
             return;
         }
+        signUpStart({displayName,email,password});
 
-        try {
-            const {user} =await auth.createUserWithEmailAndPassword(email, password);
-            createUserProfileDocument(user, {displayName});
-            this.setState({
-                displayName: '',
-                email:'',
-                password: '',
-                confirmPassword: ''
-            })
+        // try {
+            // const {user} =await auth.createUserWithEmailAndPassword(email, password);
+            // createUserProfileDocument(user, {displayName});
+            // this.setState({
+            //     displayName: '',
+            //     email:'',
+            //     password: '',
+            //     confirmPassword: ''
+            // })
 
-        } catch(error) {
-            console.error(error);
-        }
+        // } catch(error) {
+            // console.error(error);
+        // }
 
     };
 
@@ -70,6 +70,8 @@ class SignUp extends React.Component {
     }
 }
 
+const mapDispatchToProps = dispatch => ({
+    signUpStart: userCredentials => dispatch(signUpStart(userCredentials))
+})
 
-
-export default SignUp;
+export default connect (null,mapDispatchToProps)(SignUp);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -6,11 +6,22 @@ import {
   createUserProfileDocument,
   getCurrentUser
 } from "../../firebase/firebase.utils";
-import { signInSuccess, signInFailure, signOutSuccess, signOutFailure } from "./user.actions";
+import {
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  signUpSuccess,
+  signUpFailure
+} from "./user.actions";
 
-export function* getSnapshotFromUserAuth(userAuth) {
+export function* getSnapshotFromUserAuth(userAuth, additionalData) {
   try {
-    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userRef = yield call(
+      createUserProfileDocument,
+      userAuth,
+      additionalData
+    );
     const userSnapshot = yield userRef.get();
     yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
   } catch (error) {
@@ -47,12 +58,25 @@ export function* isUserAuthenticated() {
 }
 
 export function* signOut() {
-  try{
+  try {
     yield auth.signOut();
-    yield (put(signOutSuccess()))
-  } catch(error) {
+    yield put(signOutSuccess());
+  } catch (error) {
     yield put(signOutFailure(error));
   }
+}
+
+export function* signUp({ payload: { email, password, displayName } }) {
+  try {
+    const { user } = yield auth.createUserWithEmailAndPassword(email, password);
+    yield put(signUpSuccess({ user, additionalData: { displayName } }));
+  } catch (error) {
+    put(signUpFailure(error));
+  }
+}
+
+export function* signInAfterSignUp({ payload: { user, additionalData } }) {
+  yield getSnapshotFromUserAuth(user, additionalData);
 }
 
 export function* onGoogleSignInStart() {
@@ -67,7 +91,15 @@ export function* onCheckUserSession() {
 }
 
 export function* onSignOutStart() {
-  yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut)
+  yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
+
+export function* onSignUpStart() {
+  yield takeLatest(UserActionTypes.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSuccess() {
+  yield takeLatest(UserActionTypes.SIGN_UP_SUCCESS, signInAfterSignUp);
 }
 
 export function* userSagas() {
@@ -75,6 +107,8 @@ export function* userSagas() {
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
     call(isUserAuthenticated),
-    call(onSignOutStart)
+    call(onSignOutStart),
+    call(onSignUpStart),
+    call(onSignUpSuccess)
   ]);
 }
